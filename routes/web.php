3a1178a8769d1eaf9d5d80eac0ec71f7da5dc4aa
Ananda Az Haruddin Salima <azharuddinananda@gmail.com>
2024-03-04 23:33:04 +0700

    Masya Allah Tabarakallah

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,21 @@
 <?php
-use App\Http\Controllers\levelController;
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UserController;
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/level', [levelController::class, 'index']);
+
+//level index
+Route::get('/level', [LevelController::class, 'index']);
+
+//kategori
+Route::get('/kategori', [KategoriController::class, 'index']);
+
+//user Con
+Route::get('/user',[UserController::class,'index'] );

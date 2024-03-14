f1a1b8aba8c37a5acec517b5ea115b613d6b0911
Ananda Az Haruddin Salima <azharuddinananda@gmail.com>
2024-03-14 20:31:56 +0700

    jb 4

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,9 @@ Route::get('/kategori', [KategoriController::class, 'index']);
 
 //user Con
 Route::get('/user',[UserController::class,'index'] );
+
+Route::get('/user/tambah',[UserController::class,'tambah'] )->name('/user/tambah');
+Route::get('/user/ubah/{id}',[UserController::class,'ubah'])->name('/user/ubah');
+Route::get('/user/hapus/{id}',[UserController::class,'hapus'])->name ('/user/hapus');
+Route::get('/user',[UserController::class,'index'])->name ('/user');
+Route::post('/user/tambah_simpan',[UserController::class,'tambah_simpan'])->name ('/user/tambah_simpan');
